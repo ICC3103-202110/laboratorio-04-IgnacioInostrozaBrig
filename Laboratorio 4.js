f1cf78210166947fae5bfd76a85ac7b80cbc7f04
Ignacio Inostroza <iinostroza1@miuandes.cl>
2021-05-09 15:38:50 -0400

    Laboratorio 4 ver1.0

---
+++ Laboratorio 4.js
@@ -0,0 +1,38 @@
+const prompt = require('prompt-sync')({sigint: true});
+ 
+//const name = prompt('What is your name?');
+
+//Prints current value of counter
+function view(counter){
+return (`\nCount: ${counter}\nAdd(+)/Substract(-)\n`);
+}
+
+// Updates counter value depending on input
+function update(msg, counter){
+//Adds a unit to the counter value
+if (msg === "+"){
+return (counter+1);
+}
+//Subtracts a unit to the counter value
+else if (msg === "-"){
+return (counter-1);
+}
+//Invalid input, nothing happens to counter value
+else{
+return counter;
+}
+}
+
+//Brain function
+function app(counter){
+while (true){
+console.clear();
+const currentView = view(counter);
+console.log(`\n${currentView}\nWhat will you do?\n`);
+//prompt(`\n${currentView}\nWhat will you do?\n);
+const msg = prompt();
+counter = update(msg,counter);
+}
+}
+
+app(0);
